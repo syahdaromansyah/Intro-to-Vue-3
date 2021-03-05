5272ece42a03a01414e9af357c786cd76db12c69
syahdaromansyah <syahdarom@gmail.com>
2021-03-05 21:43:28 +0700

    Updated L2-start

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const product = 'Socks'
+// const product = 'Socks';
+
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Boots',
+      description:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat earum cupiditate amet? Eos beatae nihil aperiam laborum molestias perferendis necessitatibus expedita tempora ea assumenda.',
+    };
+  },
+});
+
+const mountApp = app.mount('#app');
