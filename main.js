c6e6b18a0273cd1e660f15fbfbd673a09efe62d1
syahdaromansyah <syahdarom@gmail.com>
2021-03-06 05:51:59 +0700

    Updated for completing a lesson of L3-start

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: 'Socks',
+      images: './assets/images/socks_green.jpg',
+    };
+  },
+});
