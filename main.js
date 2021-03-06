3a5b55bc64d2fe3217df510f3a91f5e462c0eeeb
syahdaromansyah <syahdarom@gmail.com>
2021-03-06 14:26:16 +0700

    Finished lesson L5

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
-    }
-})
+  data() {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_blue.jpg',
+      inStock: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'Green' },
+        { id: 2235, color: 'Blue' },
+      ],
+      sizes: ['Small', 'Medium', 'Large', 'XL', 'XXL', 'SXL', 'GXL'],
+    };
+  },
+});
