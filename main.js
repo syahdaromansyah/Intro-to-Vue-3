16b6265f6770325c1cdc7f180811340e7cb882d5
syahdaromansyah <syahdarom@gmail.com>
2021-03-06 22:07:13 +0700

    Finished lesson on L7

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  data() {
+    return {
+      cart: 0,
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      image: './assets/images/socks_blue.jpg',
+      itemRemaining: 12,
+      inStock: true,
+      details: ['50% Cotton', '30% Wool', '20% Polyester'],
+      variants: [
+        {
+          id: 2234,
+          color: '#3E9E79',
+          image: './assets/images/socks_green.jpg',
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+        { id: 2235, color: '#224C70', image: './assets/images/socks_blue.jpg' },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.itemRemaining -= 1;
+      this.cart += 1;
+      if (!this.itemRemaining) return (this.inStock = false);
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+  },
+});
