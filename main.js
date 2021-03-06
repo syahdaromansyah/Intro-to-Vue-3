bf0ede72c1974494e87bb7e5a19ffe4c895a25b5
syahdaromansyah <syahdarom@gmail.com>
2021-03-06 11:58:57 +0700

    Updated for completing the lesson

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
-        }
-    }
-})
+  data() {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_blue.jpg',
+      //   inStock: true,
+      //   inStock: false,
+      inventory: 120,
+      //   onSale: true,
+      onSale: false,
+    };
+  },
+});
